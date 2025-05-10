a3aa62009deb90fddbdcec46e4f009f3119233bb
sonarx <sonarxpolus@gmail.com>
2025-05-10 22:31:14 +0700

    First working window renderer

--- src/com/uet/oop/core/Game.java
+++ src/com/uet/oop/core/Game.java
@@ -1,9 +1,18 @@
 package com.uet.oop.core;
 
+
+import javax.swing.JFrame;
+
+
 public enum Game {
     INSTANCE;
-
+    JFrame window;
     Game() {
+        window = new JFrame();
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setVisible(true);
         System.out.println("Game initialized");
     }
 
